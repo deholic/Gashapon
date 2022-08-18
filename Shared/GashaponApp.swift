dda36883f332d0c1ad6251190443f1caa9e219f3
Euigyom Kim <deholexp@gmail.com>
2022-08-18 10:27:50 +0900

    Initial Commit

---
+++ Shared/GashaponApp.swift
@@ -0,0 +1,17 @@
+//
+//  GashaponApp.swift
+//  Shared
+//
+//  Created by Euigyom Kim on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct GashaponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
